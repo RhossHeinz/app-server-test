0bebc1bf17ebeb236e2effcd1fb7ed2ef6fcf932
RhossHeinz <apn.ronyalcala@gmail.com>
2023-07-21 05:56:26 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ api/check.js
@@ -0,0 +1,54 @@
+import { google } from "googleapis";
+const playintegrity = google.playintegrity('v1');
+
+
+const packageName = process.env.PACKAGE_NAME
+const privatekey = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+
+
+async function getTokenResponse(token) {
+
+    let jwtClient = new google.auth.JWT(
+        privatekey.client_email,
+        null,
+        privatekey.private_key,
+        ['https://www.googleapis.com/auth/playintegrity']);
+
+    google.options({ auth: jwtClient });
+
+    const res = await playintegrity.v1.decodeIntegrityToken(
+        {
+            packageName: packageName,
+            requestBody:{
+                "integrityToken": token
+            }
+        }
+
+    );
+
+
+    console.log(res.data.tokenPayloadExternal);
+
+    return res.data.tokenPayloadExternal
+}
+
+module.exports = async (req, res) => {
+
+    const { token = 'none' } = req.query
+
+    if (token == 'none') {
+        res.status(400).send({ 'error': 'No token provided' })
+        return
+    }
+
+    getTokenResponse(token)
+        .then(data => {
+            res.status(200).send(data)
+            return
+        })
+        .catch(e => {
+            console.log(e)
+            res.status(200).send({ 'error': 'Google API error.\n' + e.message })
+            return
+        });
+}
